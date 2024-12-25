e154af167a56798b5a0b426b4a9de8eeee61821b
anas ismail <msanasismail@gmail.com>
2024-12-25 14:05:10 +0530

    dc up

--- src/contexts/DataContexts.jsx
+++ src/contexts/DataContexts.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 const DataContext = React.createContext();
 
